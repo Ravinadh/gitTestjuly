4e7cff760e86a02cd8bca982942fda7c76f149ad
ravinadh <ravichda@gmail.com>
2021-07-02 07:25:26 +0530

    First commit

---
+++ src/main/java/com/qa/libs/GitLibClass.java
@@ -0,0 +1,12 @@
+package com.qa.libs;
+
+public class GitLibClass {
+	
+	
+	public static void gitLib() {
+		
+		
+		System.out.println("Hai");
+	}
+
+}
---
+++ src/test/java/com/test/gitlearning/GitLibTest.java
@@ -0,0 +1,11 @@
+package com.test.gitlearning;
+
+public class GitLibTest {
+	
+	public static void GitLibTest() {
+		
+		System.out.println("Thsi is Test Class");
+		
+	}
+
+}
